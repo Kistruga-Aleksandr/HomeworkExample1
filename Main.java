e461a26721e940d136ef78170c6516d5ae5cfa87
Александр <a@kistruga.ru>
2022-05-16 23:13:52 +0400

    Домашнее задание переменые

--- Main.java
+++ Main.java
@@ -1,5 +1,60 @@
 public class Main {
     public static void main(String[] args) {
-
+        byte Byte = 20;
+        short Short = 200;
+        int Int = 1000;
+        long Long = 10000L;
+        float Float = 10.5F;
+        double Double = 15.6;
+        double Boxer1 = 78.2;
+        double Boxer2 = 82.7;
+        double weightOfBoxers = Boxer1 + Boxer2;
+        System.out.println("Вес боксеров " + weightOfBoxers + " кг");
+        double theDifferenceInTheWeightOfBoxers = Boxer2 - Boxer1;
+        System.out.println(" Разница в весе боксеров " + theDifferenceInTheWeightOfBoxers + " кг");
+        var quantityBananasPieces = 5;
+        var quantityMilkMilliliters = 200;
+        var quantityIceCreamSundaeBriquettes = 2;
+        var quantityRawEggsPieces = 4;
+        var weight1BananasGrams = 80;
+        var weight100MillilitersMilkGrams = 105;
+        var weight1IceCreamSundaeBriquettes = 100;
+        var weight1RawEggsGrams = 70;
+        var weightBananasGrams = quantityBananasPieces * weight1BananasGrams;
+        var weightMillilitersMilkGrams = weight100MillilitersMilkGrams * quantityMilkMilliliters / 100;
+        var weightIceCreamSundaeBriquettesGrams = weight1IceCreamSundaeBriquettes * quantityIceCreamSundaeBriquettes;
+        var weightRawEggsGrams = weight1RawEggsGrams * quantityRawEggsPieces;
+        double sportsBreakfastGrams = weightBananasGrams + weightMillilitersMilkGrams + weightIceCreamSundaeBriquettesGrams + weightRawEggsGrams;
+        var sportsBreakfastKilogram = sportsBreakfastGrams / 1000;
+        System.out.println("Вес завтрака спортсмена перед тоенеровкой = " + sportsBreakfastKilogram + " кг!");
+        int needToResetGrams = 7 * 1000;// перевел килограммы в граммы.
+        int WeightLossPerDay250Grams = 250;
+        int WeightLossPerDay500Grams = 500;
+        int daysToLoseWeight250Grams = needToResetGrams / WeightLossPerDay250Grams;
+        System.out.println("Спотсмен похудее за " + daysToLoseWeight250Grams + " дней при ежедневной потере по 250 грамм");
+        int daysToLoseWeight500Grams = needToResetGrams / WeightLossPerDay500Grams;
+        System.out.println("Спотсмен похудее за " + daysToLoseWeight500Grams + " дней при ежедневной потере по 500 грамм");
+        int averageNumberDays = (daysToLoseWeight250Grams + daysToLoseWeight500Grams) / 2;
+        System.out.println("Среднне количество дне потребующихся для похудение " + averageNumberDays);
+        float salaryIncreasePercent = 1.1F; // Повышение на 10%.
+        int month = 12;
+        int Masha = 67671;
+        int Denis = 83690;
+        int Kristina = 76230;
+        float SalaryMashaAfterPromotion = Masha * salaryIncreasePercent;
+        float SalaryDenisAfterPromotion = Denis * salaryIncreasePercent;
+        float SalaryKristinaAfterPromotion = Kristina * salaryIncreasePercent;
+        double SalaryMashaPerYearAfterPromotion = SalaryMashaAfterPromotion * month;
+        double SalaryDenisPerYearAfterPromotion = SalaryDenisAfterPromotion * month;
+        double SalaryKristinaPerYearAfterPromotion = SalaryKristinaAfterPromotion * month;
+        double SalaryMashaPerYear = Masha * month;
+        double SalaryDenisPerYear = Denis * month;
+        double SalaryKristinaPerYear = Kristina * month;
+        double MashaAnnualIncomeHasGrown = SalaryMashaPerYearAfterPromotion - SalaryMashaPerYear;
+        double DenisAnnualIncomeHasGrown = SalaryDenisPerYearAfterPromotion - SalaryDenisPerYear;
+        double KristinaAnnualIncomeHasGrown = SalaryKristinaPerYearAfterPromotion - SalaryKristinaPerYear;
+        System.out.println("Маша теперь получает " + SalaryMashaAfterPromotion + " рублей. Годовой доход вырос на " + MashaAnnualIncomeHasGrown + " рублей");
+        System.out.println("Денис теперь получает " + SalaryDenisAfterPromotion + " рублей. Годовой доход вырос на " + DenisAnnualIncomeHasGrown + " рублей");
+        System.out.println("Кристина теперь получает " + SalaryKristinaAfterPromotion + " рублей. Годовой доход вырос на " + KristinaAnnualIncomeHasGrown + " рублей");
     }
 }
